461a821f9f4c1e1e8ba5a07463f6b0e308911a5b
Koji Jimba <dj.jimba@gmail.com>
2013-08-11 17:25:41 +0900

    more of like a initialization

---
+++ addpagetotab.js
@@ -0,0 +1,8 @@
+/*  FB Add Page to Tab
+ *	version 1.0.0
+ *  (c) 2013 Flagship LLC
+ *
+/*--------------------------------------------------------------------------*/
+
+jQuery('#developerAppHeader').css('display','none');
+
